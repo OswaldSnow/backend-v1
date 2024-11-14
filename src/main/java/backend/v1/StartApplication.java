9431346ac7237efdb3ef861440e12f9ae4f3e74b
OswaldSnow <OswaldSnow88@gmail.com>
2024-11-14 22:04:05 +0800

    something

--- src/main/java/backend/v1/StartApplication.java
+++ src/main/java/backend/v1/StartApplication.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StartApplication {
 
     public static void main(String[] args) {
+        // something I forget...
         SpringApplication app = new SpringApplication(StartApplication.class);
         app.run(StartApplication.class, args);
     }
